f99614ee1498f60a597b8d3cd3c230deef47a9fc
Aiace9 <ruggero90@gmail.com>
2015-12-11 10:18:45 +0100

    removed pressure computation

--- evolution_algorithm/interaction.f90
+++ evolution_algorithm/interaction.f90
@@ -13,11 +13,10 @@ module interaction
   
   contains
 
-  subroutine interazione(pos,nbody,f,upot, side, Pist)
+  subroutine interazione(pos,nbody,f,upot, side)
     ! questa sabroutine simula una scatola cubica di lato side
     ! ma permette alle particelle di allontanarsia all' infinito
     ! dall'origine in cui si trovano
-    ! adesso aggiungo anche la pressione come parametro opzionale 
     ! la pressione attualmente non è supportata, NON UTILIZZARE
     ! dovrebbe mancare un termine che va sommato e va divisa per dV
     real(kind=kr), intent(in), dimension(:,:) :: pos
@@ -25,20 +24,20 @@ module interaction
     integer, intent(in) :: nbody
     real(kind=kr), intent(out) :: upot
     real(kind=kr), intent(out), dimension(:,:) :: f
-    real(kind=kr), intent(out), optional :: Pist
+    !real(kind=kr), intent(out), optional :: Pist
     real(kind=kr), dimension(size(pos,1)) :: posij
     real(kind=kr) :: rij
     integer ::i,j
     upot = 0
     f = 0
-    Pist = 0
+    !Pist = 0
     do i=1,nbody
       do j=1,nbody
         if( i==j ) cycle
         call PBC(pos(:,i), pos(:,j), rij, posij, side, .false.)
         upot = upot + 4*eps*(rij**(-12)-rij**(-6)) 
         f(:,i) = f(:,i) + 24*eps*(2.*rij**(-14)-rij**(-8))*posij
-        if ( j > i ) Pist = Pist + dot_product(24*eps*(2.*rij**(-14)-rij**(-8))*posij,posij)
+        !if ( j > i ) Pist = Pist + dot_product(24*eps*(2.*rij**(-14)-rij**(-8))*posij,posij)
       end do
     end do
     upot = upot/2
